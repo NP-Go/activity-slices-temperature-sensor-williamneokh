990f13c965383f693ac3b7e81dc9efae62cf9845
williamneo <williamneokh@gmail.com>
2022-04-03 22:50:06 +0800

    done

--- sliecesTemperatureSensor.go
+++ sliecesTemperatureSensor.go
@@ -1,10 +1,28 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	//slices template
-	//room1 := []float64{20, 21, 23, 25, 22}
-	//room2 := []float64{27, 23, 25, 20, 30, 24}
-	//room3 := []float64{22, 23, 24, 22}
+	room1 := []float64{20, 21, 23, 25, 22}
+	room2 := []float64{27, 23, 25, 20, 30, 24}
+	room3 := []float64{22, 23, 24, 22}
 
-	//Insert code here
+	var HdbSlice [][]float64
+
+	HdbSlice = append(HdbSlice, room1, room2, room3)
+
+	for eachRoom := range HdbSlice {
+		totalTemp := 0.0
+		for i := range HdbSlice[eachRoom] {
+
+			totalTemp += HdbSlice[eachRoom][i]
+
+		}
+		avg := totalTemp / float64(len(HdbSlice[eachRoom]))
+		fmt.Printf("Room %v average temp is %.2f\n", eachRoom+1, avg)
+	}
+
 }
